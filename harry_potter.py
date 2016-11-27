b47e6679f201f58fb2d89e89d79890a64228f7ba
ValeriaOchoa <dvale14toledo@gmail.com>
2016-11-27 11:41:23 -0500

    Funcion contar palabras agregada, la funcion cuenta las palabras que se señalan (revisar error de conteo)

--- harry_potter.py
+++ harry_potter.py
@@ -1,6 +1,6 @@
 ##ESCUELA POLITECNICA NACIONAL
 ##ESCUELA DE FORMACION DE TECNOLOGOS
-##ALGORITMOS FUNDAMENTALES
+##PROGRAMACION AVANZADA
 ##AUTOR:DIANA BONILLA-VALERIA OCHOA
 ##TITULO: PRACTICA DE MANEJO DE ARCHIVOS
 
@@ -9,14 +9,33 @@ print("\t\t\t Escuela de Formación de Tecnólogos")
 print("\t\t\t\t  Programación Avanzada")
 print("\t\t\t\tDiana Bonilla - Valeria Ochoa")
 print("Cuenta el numero de ocurrencias de palabras en un archivo")
+p= "Quirrell"
 ##def creartxt():
 ##    archivo = open('HarryPotter.txt','a')
 ##    archivo.close()
-    
+
+def contarPalabras():
+    archivo=open('HarryPotter.txt','r')
+    data=archivo.readlines()
+    archivo.close()
+    contador=0
+    cont=0
+
+    for renglon in data:
+        for palabra in renglon.split(' '):
+            if palabra == "Hagrid":
+                contador +=2
+                print(str(contador),palabra)
+##                if palabra == "Harry":
+##                    cont+=1
+##                    print("Hay: ", str(cont),"con la palabra Harry")
+                
+            
 def grabartxt():
-    archivo = open('HarryPotter.txt','a')
-    archivo.write('Hola')
+    archivo = open('NumPalaHarry.txt','a')
+    archivo.write("hola")
     archivo.close()
 
 ##creartxt()
+contarPalabras()
 grabartxt()
